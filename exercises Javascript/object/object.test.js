2fdd660a8d42542697d44f818b575aef5f9731fa
David Habetyan <davidhabetyanm@gmail.com>
2022-05-13 21:40:23 +0400

    added exercises Object

--- exercises Javascript/object/object.test.js
+++ exercises Javascript/object/object.test.js
@@ -28,3 +28,44 @@ describe('Output the books sorted by the percent in descending order which readS
    })
 
 })
+
+
+describe('Create a function called transform which takes an array of objects of this form:', () => {
+   function transform(arr) {
+      return arr.filter((val) => {
+         return val.gender === "female"
+      }).map((val) => {
+         return{
+            firstName: val.fn,
+            lastName: val.ln,
+            phone: val.ph,
+            gender: val.gender
+         }
+      })
+   }
+
+   test('Test 1. The function should filter the list down to only those whose gender value is “female” and return an array with those objects transformed into the following form:', () => {
+      expect(transform([
+         {fn: "Joe", ln: "Mikaelyan", ph: "039 323 233", gender: "male" },
+         {fn: "Mike", ln: "Hovsepyan", ph: "039 323 233", gender: "male" },
+         {fn: "Jane", ln: "Karapetyan", ph: "039 323 233", gender: "female" },
+         {fn: "Jill", ln: "Isahakyan", ph: "039 323 233", gender: "female" }
+      ])).toEqual([
+            {
+               firstName: 'Jane',
+               lastName: 'Karapetyan',
+               phone: '039 323 233',
+               gender: 'female'
+            },
+            {
+               firstName: 'Jill',
+               lastName: 'Isahakyan',
+               phone: '039 323 233',
+               gender: 'female'
+            }
+         ]
+      )
+
+   })
+
+})
